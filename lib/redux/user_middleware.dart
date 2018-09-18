dbd004b351fd58e41f6971203a99e112185ef61e
pablo garzon <pablofgarzon@gmail.com>
2018-09-18 17:47:03 -0300

    created the user middleware

---
+++ lib/redux/user_middleware.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'dart:async';
+
+import 'package:redux/redux.dart';
+
+import 'app_state.dart';
+import 'user_actions.dart';
+
+import 'package:randomuser_flutter_app/models/user.dart';
+
+class PersonMiddleware extends MiddlewareClass<AppState> {
+
+  @override
+  Future<void> call(Store<AppState> store, dynamic action, NextDispatcher next) async{
+    next(action);
+
+    if(action is FetchUsersAction){
+      var users = <User>[];
+      final response = await http.get('http://10.0.2.2:3000/data', headers: {"Accept": "application/json"});
+      if(response.statusCode == 200) {
+        var data = json.decode(response.body);
+        for (var elem in data) {
+          users.add(User.fromJson(elem));
+        }
+        store.dispatch(ReceivedUsersAction(users: users));
+      } else {
+        throw Exception('Failed to load data');
+      }
+    }
+  }
+}
+
