5137d656ebe6275c050a947fa0de84ba605b757d
pablo garzon <pablofgarzon@gmail.com>
2018-09-19 13:18:46 -0300

    fix the user middleware

--- lib/redux/user_middleware.dart
+++ lib/redux/user_middleware.dart
@@ -13,17 +13,15 @@ class UserMiddleware extends MiddlewareClass<AppState> {
 
   @override
   Future<void> call(Store<AppState> store, dynamic action, NextDispatcher next) async{
-    next(action);
-
     if(action is FetchUsersAction){
       var users = <User>[];
       final response = await http.get('https://randomuser.me/api/?results=50', headers: {"Accept": "application/json"});
       if(response.statusCode == 200) {
         var data = json.decode(response.body);
-        for (var elem in data) {
+        for (var elem in data["results"]) {
           users.add(User.fromJson(elem));
         }
-        store.dispatch(ReceivedUsersAction(users: users));
+        next(ReceivedUsersAction(users: users));
       } else {
         throw Exception('Failed to load data');
       }
