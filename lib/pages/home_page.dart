6ae3c93c0c395051193f8c04e969fc9ec52a044d
pablo garzon <pablofgarzon@gmail.com>
2018-09-19 16:30:56 -0300

    added storebuilder to homepage, deleted property on viewmodel

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -20,44 +20,46 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("Users"),
       ),
-      body: StoreConnector<AppState, _HomePageViewModel>(
-        converter: (Store<AppState> store) => _HomePageViewModel.fromStore(store),
-        builder: (BuildContext context, _HomePageViewModel vm){
-          vm.refreshEvents();
-          return ListView.builder(
-            itemBuilder: (BuildContext context, int index){
-              return Container(
-                margin: EdgeInsets.all(5.0),
-                height: 50.0,
-                width: 90.0,
-                child: ListTile(
-                  leading: CircleAvatar(
-                    child: Image(image: NetworkImage(vm.users[index].picture.thumbnail)),
-                  ),
-                  title: Center(
-                    child: Text(vm.users[index].email),
-                  ),
-                )
+      body: StoreBuilder(
+        onInit: (store) =>  store.dispatch(FetchUsersAction()),
+        builder: (BuildContext context, Store<AppState> store) {
+          return StoreConnector<AppState, _HomePageViewModel>(
+            converter: (Store<AppState> store) => _HomePageViewModel.fromStore(store),
+            builder: (BuildContext context, _HomePageViewModel vm){
+              return ListView.builder(
+                itemBuilder: (BuildContext context, int index){
+                  return Container(
+                      margin: EdgeInsets.all(5.0),
+                      height: 50.0,
+                      width: 90.0,
+                      child: ListTile(
+                        leading: CircleAvatar(
+                          child: Image(image: NetworkImage(vm.users[index].picture.thumbnail)),
+                        ),
+                        title: Center(
+                          child: Text(vm.users[index].email),
+                        ),
+                      )
+                  );
+                },
+                itemCount: vm.users.length,
               );
             },
-            itemCount: vm.users.length,
           );
-        },
-      ),
+        }
+      )
     );
   }
 }
 
 class _HomePageViewModel {
   final List<User> users;
-  final Function refreshEvents;
 
-  _HomePageViewModel({this.users, this.refreshEvents});
+  _HomePageViewModel({this.users});
 
   factory _HomePageViewModel.fromStore(Store<AppState> store) {
     return _HomePageViewModel(
-        users: store.state.users,
-        refreshEvents: () => store.dispatch(FetchUsersAction())
+        users: store.state.users
     );
   }
 
