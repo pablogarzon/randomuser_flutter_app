c308766bb7bfcec99c339c24c924a4b83aa454a0
pablo garzon <pablofgarzon@gmail.com>
2018-09-20 17:42:50 -0300

    added gender filter layout

--- lib/pages/filters_page.dart
+++ lib/pages/filters_page.dart
@@ -6,12 +6,56 @@ class FiltersPage extends StatefulWidget {
 }
 
 class _FiltersPageState extends State<FiltersPage> {
+
+  String _gender;
+  double _discreteValue = 20.0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Filters"),
+        actions: <Widget>[
+          FlatButton(
+            onPressed: null, 
+            child: Text("Apply") 
+          )
+        ],
       ),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Text("Gender:", style: TextStyle(fontSize: 18.0),),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Radio(value: "all", groupValue: _gender, onChanged: null),
+                        Text("All"),
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Radio(value: "all", groupValue: _gender, onChanged: null),
+                        Text("Male"),
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Radio(value: "all", groupValue: _gender, onChanged: null),
+                        Text("Female"),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      )
     );
   }
 }
