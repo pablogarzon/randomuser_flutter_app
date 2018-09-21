e8919bcee9c1baa4551cde66379d28e6b5c61bb2
pablo garzon <pablofgarzon@gmail.com>
2018-09-21 10:09:36 -0300

    updated user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,18 +1,32 @@
 class User {
-  Login login;
   String gender;
   String email;
+  Login login;
   Name name;
+  Dob dob;
   Picture picture;
+  String nat;
 
   User();
 
   User.fromJson(Map json){
-    this.login = Login.fromJson(json["login"]);
     this.gender = json["gender"];
     this.email = json["email"];
+    this.login = Login.fromJson(json["login"]);
     this.name = Name.fromJson(json["name"]);
+    this.dob = Dob.fromJson(json["dob"]);
     this.picture = Picture.fromJson(json["picture"]);
+    this.nat = json["nat"];
+  }
+}
+
+class Name {
+  String first;
+  String last;
+
+  Name.fromJson(Map json){
+    this.first = json["first"];
+    this.last = json["last"];
   }
 }
 
@@ -26,13 +40,11 @@ class Login {
   }
 }
 
-class Name {
-  String first;
-  String last;
+class Dob {
+  int age;
 
-  Name.fromJson(Map json){
-    this.first = json["first"];
-    this.last = json["last"];
+  Dob.fromJson(Map json){
+    this.age = json["age"];
   }
 }
 
